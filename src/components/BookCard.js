6c92b05db3a6abea9e36572a5939b6c3de860c8a
clavierbulb-green <clavierbulb-green@protonmail.com>
2020-03-03 11:06:13 -0800

    Edit Book Card styles

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -8,7 +8,7 @@ class BookCard extends React.Component {
     return (
       <div className="book-card">
         <h2 className="title">{this.props.book.title}</h2>
-        <p>{this.props.book.author}</p>
+        <p className="author">{this.props.book.author}</p>
         <p>{this.props.book.numPages} pages</p>
         <button className="remove-book-button"
     onClick={this.props.handleRemove.bind(this, this.props.book.id)}>X</button>
